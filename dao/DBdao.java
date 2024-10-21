794f703ced7cb6ae876b128f91a7f2e2ccf9d306
J1NU2 <pj1nu2@gmail.com>
2024-10-21 11:29:47 +0900

    DB 인터페이스 추가

--- dao/DBdao.java
+++ dao/DBdao.java
@@ -15,12 +15,15 @@ public interface DBdao {
 	public boolean patientIdenCheck(String findId);					// 회원가입 시 주민번호 중복 체크
 	
 	// 관계자(의사)
-	public void doctorSignUp(DoctorDTO doctordto);					// 관계자 등록
+	public void doctorSignUp(DoctorDTO doctordto);		// 관계자 등록
 	
 	// 예약관련
-	public ArrayList<DoctorDTO> doctorAll();						// 예약 시 의사 목록
-	public DoctorDTO doctorOne(String findNum);						// 예약 시 의사를 선택하면 해당 의사의 정보 리턴
-	public void reservationAdd(ReservationDTO reservdto);			// 예약하기
-	public ReservationDTO selectOne(PatientDTO patientdto);			// 일반회원이 현재 예약한 내역
-	public ArrayList<ReservationDTO> selectAll();					// 예약한 모든 내역
+	public ArrayList<DoctorDTO> doctorAll();							// 예약 시 의사 목록
+	public DoctorDTO doctorOne(String findNum);							// 예약 시 의사를 선택하면 해당 의사의 정보 리턴
+	public void reservationAdd(ReservationDTO reservdto);				// 예약하기
+	public ReservationDTO reservationCurrent(String findIden);			// 일반회원이 현재 예약한 내역
+	public ReservationDTO reservationCurrentDoctor(String findDate, String findTime, String findDoc);	// 선택한 의사에게 예약한 날짜/시간이 겹치는 내역
+	public void reservationDel(String findIden);						// 현재 예약 취소
+	public ArrayList<ReservationDTO> reservationPatientAll(String findIden);	// 로그인한 환자가 예약한 모든 내역
+	public ArrayList<ReservationDTO> reservationDoctorAll(String findDoc);		// 로그인한 의사에게 예약한 모든 내역
 }
